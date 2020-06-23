f2583d4278e8d4f43c5921739a6c5b54ec38f7d0
vddCore <vddcore@outlook.com>
2020-06-23 11:06:33 +0200

    Add custom cursor support and the corresponding example.

--- Chroma/Input/Cursor.cs
+++ Chroma/Input/Cursor.cs
@@ -1,19 +1,37 @@
 ﻿using System;
 using System.Numerics;
 using Chroma.Diagnostics.Logging;
+using Chroma.Graphics;
 using Chroma.MemoryManagement;
 using Chroma.Natives.SDL;
 
-namespace Chroma.Graphics
+namespace Chroma.Input
 {
     public class Cursor : DisposableResource
     {
+        private static bool _isVisible = true;
+        
         private IntPtr _cursorHandle;
 
         private Log Log { get; } = LogManager.GetForCurrentAssembly();
 
+        public static bool IsVisible
+        {
+            get => _isVisible;
+            set
+            {
+                _isVisible = value;
+                
+                if(_isVisible)
+                    SDL2.SDL_ShowCursor(1);
+                else SDL2.SDL_ShowCursor(0);
+            }
+        }
+        
         public Texture Texture { get; }
         public Vector2 HotSpot { get; }
+        
+        public bool IsCurrent => SDL2.SDL_GetCursor() == _cursorHandle;
 
         public Cursor(string imageFilePath, Vector2 hotSpot)
         {
@@ -41,9 +59,7 @@ namespace Chroma.Graphics
         }
 
         public static void Reset()
-        {
-            SDL2.SDL_SetCursor(IntPtr.Zero);
-        }
+            => SDL2.SDL_SetCursor(SDL2.SDL_GetDefaultCursor());
 
         protected override void FreeNativeResources()
         {
@@ -52,9 +68,7 @@ namespace Chroma.Graphics
         }
 
         protected override void FreeManagedResources()
-        {
-            Texture.Dispose();
-        }
+            => Texture.Dispose();
 
         private void Initialize()
         {
